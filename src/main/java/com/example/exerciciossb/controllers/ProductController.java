c01861f188f51f090aa609f427d778951fff3f72
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-08 19:18:50 -0300

    Use of Read and Update using Spring Boot

--- src/main/java/com/example/exerciciossb/controllers/ProductController.java
+++ src/main/java/com/example/exerciciossb/controllers/ProductController.java
@@ -4,10 +4,15 @@ import com.example.exerciciossb.model.entities.Product;
 import com.example.exerciciossb.model.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/products")
@@ -19,6 +24,26 @@ public class ProductController {
     // Pode passar diretamente o objeto, já que ele analisa o construtor da Entidade
     @PostMapping
     public @ResponseBody Product newProduct(@Valid Product product){
+        productRepository.save(product);
+        return product;
+    }
+
+    // Consulta de Dados no Banco de todos os produtos, sem paginação
+    // Não é uma boa Implementação
+    @GetMapping
+    public Iterable<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    // Consulta por ID
+    @GetMapping(path = "{id}")
+    public Optional<Product> getProductById(@PathVariable int id){
+        return productRepository.findById(id);
+    }
+
+    // Update do Produto com um novo metodo
+    @PutMapping
+    public Product updateProduct(@Valid Product product){
         productRepository.save(product);
         return product;
     }
--- src/main/java/com/example/exerciciossb/model/repositories/ProductRepository.java
+++ src/main/java/com/example/exerciciossb/model/repositories/ProductRepository.java
@@ -4,6 +4,7 @@ import com.example.exerciciossb.model.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
 // Não é possível usar tipos primitivos
+// É uma abstração para manipulação com o banco de dados
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
 }
