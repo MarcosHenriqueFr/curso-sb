b083689e3822019b07329f7a912fd57c129a6a3d
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-08 20:10:57 -0300

    Simple Queries in Repositories

--- src/main/java/com/example/exerciciossb/model/repositories/ProductRepository.java
+++ src/main/java/com/example/exerciciossb/model/repositories/ProductRepository.java
@@ -2,10 +2,15 @@ package com.example.exerciciossb.model.repositories;
 
 import com.example.exerciciossb.model.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 // Não é possível usar tipos primitivos
 // É uma abstração para manipulação com o banco de dados
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     public Iterable<Product> findByNameContainingIgnoreCase(String nameSection);
+
+    @Query("SELECT p FROM products p WHERE p.name LIKE %:name%")
+    public Iterable<Product> searchByNameLike(@Param("name") String name);
 }
